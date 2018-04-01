cfde292b75c88d6a7b6e00e4a278d94cd7661dbf
Tim Igoe <tim@tsihosting.co.uk>
2018-04-01 23:38:52 +0100

    Start to update library

--- src/Xtroo.php
+++ src/Xtroo.php
@@ -3,6 +3,11 @@
   namespace Xtroo;
 
   use Xtroo\Exceptions\XtrooException;
+  use Http\Discovery\HttpClientDiscovery;
+  use Http\Discovery\MessageFactoryDiscovery;
+  use Http\Client\Common\HttpMethodsClient;
+  use Http\Client\Common\PluginClient;
+  use Http\Client\Common\Plugin\HeaderSetPlugin;
 
 
   /**
@@ -16,6 +21,10 @@
   {
     protected $token = null;
 
+    protected $urlEndpooint = 'http://public.xtroo.dev.tsihosting.co.uk/api/';
+
+    private $client = null;
+
     function __construct($token = null)
     {
       if ($token == null)
@@ -24,8 +33,90 @@
       $this->token = $token;
     }
 
+    /**
+     * Get the set token
+     *
+     * @return null|string
+     */
     public function getToken()
     {
       return $this->token;
+    }
+
+    /**
+     * Set up / Get the HTTP Client
+     *
+     * @param HttpMethodsClient|null $client
+     *
+     * @return $this
+     */
+    public function httpClient(HttpMethodsClient $client = null)
+    {
+      if ($client === null && $this->client === null)
+      {
+        $headerSetPlugin = new HeaderSetPlugin([
+          'api' => $this->token,
+        ]);
+
+        $client = HttpClientDiscovery::find();
+
+        $client = new PluginClient(
+          $client,
+          [
+            $headerSetPlugin
+          ]
+      );
+
+        $client = new HttpMethodsClient(
+          $client,
+          MessageFactoryDiscovery::find()
+        );
+      }
+
+      $this->client = $client;
+
+      return $this->client;
+    }
+
+    /**
+     * Actually go ahead and make the call
+     *
+     * @param $endpoint
+     * @param $params
+     * @throws XtrooException
+     */
+    private function makeCall($endpoint, $params)
+    {
+      $client = $this->httpClient();
+
+      $querystring = '?';
+
+      foreach ($params as $field => $value)
+        $querystring .= '&' . $field . '=' . urlencode($value);
+
+      $response = $client->get($this->urlEndpooint . $endpoint . $querystring);
+
+      $json = json_decode($response->getBody()->getContents());
+
+      if (!is_object($json))
+        throw new XtrooException('Invalid URL or URL problem.');
+
+      return $json;
+    }
+
+    /**
+     * Get an articles content
+     *
+     * @param $url
+     * @return mixed
+     * @throws XtrooException
+     */
+    public function getContent($url)
+    {
+      $json = $this->makeCall('content', [
+        'url' => $url
+      ]);
+
+      return $json;
     }
   }
